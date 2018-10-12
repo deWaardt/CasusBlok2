3d2fc1ae5c899cb69cdd8b77fe4ccce3ec83c3bf
Pascal Kessel <1526766kessel@zuyd.nl>
2018-10-12 13:17:33 +0200

    Alles toegevoegd

---
+++ CasusBlok2Main/Views/WachtwoordChange.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CasusBlok2Main.Views
+{
+    /// <summary>
+    /// Interaction logic for Meldingen.xaml
+    /// </summary>
+    public partial class Meldingen : Window
+    {
+        public Meldingen()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
